2a6252417b0aecc2564d52102041a0ff725ed27b
francisokesola <francisokesola@gmail.com>
2026-04-02 10:34:37 +0100

    Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import HeroPage from "./Homepage/page";
 import AboutPage from "./About/page";
 import Portfolio from "./Porfolio/page";
 import Contact from "./Contact/page";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+  title: "Francis Okesola's Portfolio",
+  description: "A portfolio website built for easy connectivity between clients and developer"
+}
 
 
 export default function Home() {
