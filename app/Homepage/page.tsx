78d5b80c19dd0c386692cada8f1a7002062daf13
francisokesola <francisokesola@gmail.com>
2026-04-06 16:14:51 +0100

    Error fix

--- app/Homepage/page.tsx
+++ app/Homepage/page.tsx
@@ -1,4 +1,4 @@
-import { Link } from "lucide-react"
+
 import Image from "next/image"
 
 export default function HeroPage() {
@@ -26,7 +26,9 @@ export default function HeroPage() {
                 </p>
                 <div className="flex justify-center md:justify-start">
                   <button className="bg-green-800 text-white py-2 px-4 hover:bg-green-600 border-0 rounded-lg mt-2 cursor-pointer">
-                    <Link href="#contact">Get In Touch</Link>
+                    <a href="#contact" className="flex flex-row items-center gap-3">
+                      Contact Us 
+                    </a>
                   </button>
                 </div>
             </div>
